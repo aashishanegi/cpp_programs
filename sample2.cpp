62cd5cf79a33f84e69993984153585a62d7f1306
Aashisha Negi <500091664@stu.upes.ac.in>
2023-06-13 17:33:51 +0530

    Square of a number

---
+++ sample2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number";
+    cin>>a;
+    int square=a*a;
+    cout<< "Square is:" <<square;
+    return 0;
+}
