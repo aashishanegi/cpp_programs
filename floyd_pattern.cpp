16463c24002710bede8188d40e3324e1e280e0d8
Aashisha Negi <500091664@stu.upes.ac.in>
2023-08-03 21:43:39 +0530

    Create floyd_pattern.cpp

---
+++ floyd_pattern.cpp
@@ -0,0 +1,39 @@
+// C++ program to print
+// Floyd's pattern triangle
+// using for loop
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_patt(int row)
+{
+	// Initializing count to 1.
+	int count = 1;
+
+	// The outer loop maintains
+	// the number of rows.
+	for (int i = 1; i <= row; i++)
+	{
+		// The inner loop maintains the
+		// number of column.
+		for (int j = 1; j <= i; j++)
+		{
+			// To print the numbers
+			cout << count << " ";
+
+			// To keep increasing the count
+			// of numbers
+			count += 1;
+		}
+	
+		// To proceed to next line.
+		cout << "\n";
+	}
+}
+
+// Driver Code
+int main()
+{
+	int row = 5;
+	print_patt(row);
+	return 0;
+}
