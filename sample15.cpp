5ae86ca03e7f3b6f3c35667e554702a06722dc44
Aashisha Negi <500091664@stu.upes.ac.in>
2023-08-15 23:09:10 +0530

    for loop in vectors

---
+++ sample15.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    //for loop in vectors
+    vector <int> v;
+    cout<<"Enter the elements: "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        int e;
+        cin>>e;
+        v.push_back(e);
+    }
+    cout<<"The vector is: "<<endl;
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
