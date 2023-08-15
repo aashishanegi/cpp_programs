15a90cbb61e4eda7c39ec839f37dbe17868c73b0
Aashisha Negi <500091664@stu.upes.ac.in>
2023-08-15 23:28:59 +0530

    number of occurances in vectors

---
+++ sample17.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{ vector<int> v(6);
+cout<<"Enter elements: "<<endl;
+for(int i=0;i<6;i++)
+    {
+        cin>>v[i];
+    }
+    cout<<"Enter x"<<endl;
+    int x;
+    cin>>x;
+    
+    int occ=0;
+    for(int ele:v)
+    {
+        if(ele==x)
+        {
+            occ++;
+        }
+    }
+    cout<<"Number of times: "<<occ<<endl;
+    return 0;
+}
