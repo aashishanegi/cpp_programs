1c110f275912a9b9f9316ccb09860271e39a5b71
Aashisha Negi <500091664@stu.upes.ac.in>
2023-08-27 09:39:11 +0530

    series sum

---
+++ sample34.cpp
@@ -0,0 +1,24 @@
+//s=1-2+3-4....n
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the limit: "<<endl;
+    cin>>n;
+    cout<<"The sum is: "<<endl;
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            sum-=i;
+        }
+        else
+        {
+            sum+=i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
