3ef67588d5022089d9b132d952ea037273022ad0
Aashisha Negi <500091664@stu.upes.ac.in>
2023-09-01 23:46:40 +0530

    Print name

--- sample1.cpp
+++ sample1.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main() {
 
-   string n;
+   string name;
    cout<<"Enter your name";
-   getline(cin,n);
-   cout<<n;
+   getline(cin,name);
+   cout<<name;
 }
