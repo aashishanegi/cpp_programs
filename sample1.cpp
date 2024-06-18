7229a38832e592df324f6de358f577180a95252a
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-18 20:48:38 +0530

    Update sample1.cpp

--- sample1.cpp
+++ sample1.cpp
@@ -8,3 +8,14 @@ int main() {
    getline(cin,name);
    cout<<name;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+   string name;
+   cout<<"Enter your name";
+   getline(cin,name);
+   cout<<name;
+}
