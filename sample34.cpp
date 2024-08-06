d7be7e3e5c993af2f6349ab8cb7db66b6108831c
Aashisha Negi <500091664@stu.upes.ac.in>
2024-08-06 22:40:22 +0530

    Update sample34.cpp

--- sample34.cpp
+++ sample34.cpp
@@ -22,3 +22,26 @@ int main()
     cout<<sum;
     return 0;
 }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the limit: "<<endl;
+    cin>>n;
+    cout<<"The sum is: "<<endl;
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            sum-=i;
+        }
+        else
+        {
+            sum+=i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
