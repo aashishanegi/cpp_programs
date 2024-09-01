b96f7f1cd192a3069937b023957b36e268ebe23f
Aashisha Negi <500091664@stu.upes.ac.in>
2024-09-01 21:31:27 +0530

    Create fibonacciwithrecursion.cpp

---
+++ fibonacciwithrecursion.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        // code here
+        if(n<=1) return n;
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
