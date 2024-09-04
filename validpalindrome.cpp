f108023feae4d2a1a21448c8ff38254101cd6645
Aashisha Negi <500091664@stu.upes.ac.in>
2024-09-04 19:23:23 +0530

    Create validpalindrome.cpp

---
+++ validpalindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.length()-1;
+         while(start <= end) {
+            if(!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+
+            if(!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+
+            if(tolower(s[start]) != tolower(s[end])) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+    return true;       
+    }
+};
