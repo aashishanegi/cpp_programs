1feebbdddb35c48245d3f1076488c5b71b69e799
Aashisha Negi <500091664@stu.upes.ac.in>
2024-11-01 12:23:13 +0530

    Create detectcapital.cpp

---
+++ detectcapital.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length();
+        bool a = false;
+        if(isupper(word[0])){
+            if(n>1 && isupper(word[1])){
+                a=true;
+            }
+        }
+        for(int i=1;i<n;i++){
+            if(a==false){
+                if(isupper(word[i])){
+                    return false;
+                }
+            }
+            else{
+                if(islower(word[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
